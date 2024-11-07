719c5e2de46adde5d8deff05131d04cf01441aba
tomaszaba <tomas.zaba@outlook.com>
2024-11-08 00:03:12 +0200

    Probit method

---
+++ R/prevalence_probit_method.R
@@ -0,0 +1,102 @@
+#'
+#'
+#'
+#' @keywords internal
+#'
+#'
+apply_probit_method <- function(x, .status = c("gam", "sam")) {
+
+  ## Enforce options in `.status` ----
+  .status <- match.arg(.status)
+
+  ## Calculate mean of zscores ----
+  mean <- mean(remove_flags(x, "zscores"), na.rm = TRUE)
+
+  ## Estimate prevalence based on probit method, with a SD = 1 ----
+  switch(
+    .status,
+    "gam" = {pnorm(q = -2, mean = mean, sd = 1, lower.tail = TRUE, log.p = FALSE)},
+    "sam" = {pnorm(q = -3, mean = mean, sd = 1, lower.tail = TRUE, log.p = FALSE)}
+  )
+}
+
+
+
+#'
+#'
+#' @keywords internal
+#'
+#'
+estimate_probit_prevalence <- function(df,
+                                      .by = NULL,
+                                      .for = c("wfhz", "mfaz")) {
+
+  ## Difuse argument ----
+  .by <- enquo(.by)
+
+  ## Enfornce options in `.for` ----
+  .for <- match.arg(.for)
+
+  ## Calculate probit-based prevalence ----
+  switch(
+    .for,
+    "wfhz" = {
+      if(!is.null(.by)) {
+        df <- df |>
+          summarise(
+            gam = apply_probit_method(.data$wfhz, .status = "gam"),
+            sam = apply_probit_method(.data$wfhz, .status = "sam"),
+            mam = .data$gam - .data$sam,
+            .by = !!.by
+          ) |>
+          mutate(
+            gam_p = .data$gam, sam_p = .data$sam, mam_p = .data$mam,
+            gam = NA, sam = NA, mam = NA
+          ) |>
+          select(!1:3)
+      } else {
+        df <- df |>
+          summarise(
+            gam = apply_probit_method(.data$wfhz, .status = "gam"),
+            sam = apply_probit_method(.data$wfhz, .status = "sam"),
+            mam = .data$gam - .data$sam
+          ) |>
+          mutate(
+            gam_p = .data$gam, sam_p = .data$sam, mam_p = .data$mam,
+            gam = NA, sam = NA, mam = NA
+          ) |>
+          select(!1:3) ## To make it fit in the structure of the df
+      }
+      df
+    },
+    "mfaz" = {
+      if(!is.null(.by)) {
+        df <- df |>
+          summarise(
+            gam = apply_probit_method(.data$mfaz, .status = "gam"),
+            sam = apply_probit_method(.data$mfaz, .status = "sam"),
+            mam = .data$gam - .data$sam,
+            .by = !!.by
+          ) |>
+          mutate(
+            gam_p = .data$gam, sam_p = .data$sam, mam_p = .data$mam,
+            gam = NA, sam = NA, mam = NA
+          ) |>
+          select(!1:3)
+      } else {
+        df <- df |>
+          summarise(
+            gam = apply_probit_method(.data$mfaz, .status = "gam"),
+            sam = apply_probit_method(.data$mfaz, .status = "sam"),
+            mam = .data$gam - .data$sam
+          ) |>
+          mutate(
+            gam_p = .data$gam, sam_p = .data$sam, mam_p = .data$mam,
+            gam = NA, sam = NA, mam = NA
+          ) |>
+          select(!1:3) ## To make it fit in the structure of the df
+      }
+      df
+    }
+  )
+}
---
+++ tests/testthat/test-prevalence_probit_method.R
@@ -0,0 +1,66 @@
+# Test check: apply_probit_method() ----
+testthat::test_that(
+  "apply_probit_approach works",
+  {
+    ## Input data ----
+    x <- anthro.03 |>
+      mw_wrangle_wfhz(sex, weight, height, .recode_sex = TRUE) |>
+      subset(district == "Metuge")
+
+    ## Observed results ----
+    p_gam <- apply_probit_method(x$wfhz, .status = "gam")
+    p_sam <- apply_probit_method(x$wfhz, .status = "sam")
+
+    ## Tests ----
+    testthat::expect_vector(c(p_gam, p_sam), ptype = double(), size = 2)
+  }
+)
+
+# Test check: estimate_probit_prevalence() ----
+testthat::test_that(
+  "estimate_probit_prevalence works OK",
+  {
+    ## Input data ----
+    p <- anthro.03 |>
+      mw_wrangle_wfhz(sex, weight, height, .recode_sex = TRUE) |>
+      subset(district == "Metuge") |>
+      estimate_probit_prevalence(.for = "wfhz")
+
+    ## Tests ----
+    testthat::expect_s3_class(p, class = "tbl", exact = FALSE)
+    testthat::expect_length(p, 3)
+    testthat::expect_vector(p, nrow(1))
+  }
+)
+
+# Test check: estimate_probit_prevalence() ----
+## When `.by` is not NULL ----
+testthat::test_that(
+  "estimate_probit_prevalence works OK",
+  {
+    ## Input data ----
+    p <- anthro.03 |>
+      mw_wrangle_wfhz(sex, weight, height, .recode_sex = TRUE) |>
+      subset(district == "Metuge" | district == "Maravia") |>
+      estimate_probit_prevalence(.by = district, .for = "wfhz")
+
+    ## Tests ----
+    testthat::expect_length(p, 4)
+    testthat::expect_vector(p, nrow(2))
+  }
+)
+
+# Test check: estimate_probit_prevalence() ----
+testthat::test_that(
+  "estimate_probit_prevalence works OK with `.for` set to 'mfaz'",
+  {
+    ## Input data ----
+    p <- mfaz.01 |>
+      estimate_probit_prevalence(.for = "mfaz")
+
+    ## Tests ----
+    testthat::expect_s3_class(p, class = "tbl", exact = FALSE)
+    testthat::expect_length(p, 3)
+    testthat::expect_vector(p, nrow(1))
+  }
+)
