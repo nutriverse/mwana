ee8251abdcb237b7d531674641f77b46479f9ef1
tomaszaba <tomas.zaba@outlook.com>
2025-06-24 21:32:21 +0200

    new function `mw_stattest_ageratio2()`

--- R/stattests.R
+++ R/stattests.R
@@ -4,24 +4,27 @@
 #'
 #' @description
 #' Calculate the observed age ratio of children aged 24 to 59 months old over
-#' those aged 6 to 23 months old and test if there is a statistically 
+#' those aged 6 to 23 months old and test if there is a statistically
 #' significant difference between the observed and the expected.
 #'
 #' @param age A `numeric` vector of child's age in months.
 #'
+#' @param age_cat A `character` vector of child's age in categories. Code values 
+#' should be "6-23" and "24-59".
+#'
 #' @param .expectedP The expected proportion of children aged 24 to 59 months
-#' old over those aged 6 to 23 months old. By default, this is expected to be 
+#' old over those aged 6 to 23 months old. By default, this is expected to be
 #' 0.66.
 #'
 #' @returns A `list` object with three elements: `p` for p-value of the
-#' difference between the observed and the expected proportion of children aged 
-#' 24 to 59 months old over those aged 6 to 23 months old, `observedR` for the 
+#' difference between the observed and the expected proportion of children aged
+#' 24 to 59 months old over those aged 6 to 23 months old, `observedR` for the
 #' observed ratio, and `observedP` for the observed proportion.
 #'
 #' @details
-#' This function should be used specifically when assessing the quality of MUAC 
-#' data. For age ratio test of children aged 6 to 29 months old over 30 to 59 
-#' months old, as performed in the SMART plausibility check, use 
+#' This function should be used specifically when assessing the quality of MUAC
+#' data. For age ratio test of children aged 6 to 29 months old over 30 to 59
+#' months old, as performed in the SMART plausibility check, use
 #' [nipnTK::ageRatioTest()] instead.
 #'
 #' @references
@@ -29,10 +32,13 @@
 #' <https://smartmethodology.org/survey-planning-tools/updated-muac-tool/>
 #'
 #' @examples
+#'
 #' mw_stattest_ageratio(
 #'   age = anthro.02$age,
 #'   .expectedP = 0.66
 #' )
+#'
+#' @rdname age_ratio
 #'
 #' @export
 #'
@@ -40,7 +46,7 @@ mw_stattest_ageratio <- function(age, .expectedP = 0.66) {
   ## Enforce the class of `age` ----
   if (!is.numeric(age)) {
     stop(
-      "`age` must be of class numeric not ", class(age), 
+      "`age` must be of class numeric not ", class(age),
       ". Please try again."
     )
   }
@@ -50,6 +56,57 @@ mw_stattest_ageratio <- function(age, .expectedP = 0.66) {
   sum_o24 <- sum(na.omit(x == 1))
   sum_u24 <- sum(na.omit(x == 2))
   total <- sum(table(na.omit(x)))
+  ratio <- sum_o24 / sum_u24
+  prop <- sum_o24 / total
+
+  ## Stats test with Yates continuity correction set to false ----
+  test <- stats::prop.test(sum_o24, total, p = .expectedP, correct = FALSE)
+
+  ## Return ----
+  list(
+    p = test$p.value,
+    observedR = ratio,
+    observedP = prop
+  )
+}
+
+
+#'
+#'
+#' @examples
+#'
+#' age <- ifelse(anthro.02$age < 24, "6-23", "24-59")
+#'
+#' mw_stattest_ageratio2(
+#'   age = age,
+#'   .expectedP = 0.66
+#' )
+#'
+#' @rdname age_ratio
+#'
+#' @export
+#'
+mw_stattest_ageratio2 <- function(age_cat, .expectedP = 0.66) {
+  ## Enforce the class of `age_cat` ----
+  if (!is.character(age_cat)) {
+    stop(
+      "`age_cat` must be of class `character` not ", class(age_cat),
+      ". Please try again."
+    )
+  }
+
+  ## Enforce that categories in `age_cat` are as expected ----
+  if (!all(levels(as.factor(age_cat)) %in% c("6-23", "24-59"))) {
+    stop(
+      'Unexpected categories in `age_cat`. Please consider re-coding it to "6-23"
+      and "24-59". Please try again.'
+    )
+  }
+
+  ## Calculate observed proportion and ratio ----
+  sum_o24 <- sum(na.omit(age_cat == "24-59"))
+  sum_u24 <- sum(na.omit(age_cat == "6-23"))
+  total <- sum(table(na.omit(age_cat)))
   ratio <- sum_o24 / sum_u24
   prop <- sum_o24 / total
 
